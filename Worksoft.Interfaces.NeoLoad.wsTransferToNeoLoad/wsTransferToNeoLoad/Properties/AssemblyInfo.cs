ca519d487e04e91aa58c645b1cca487f82edc707
alexis nouvel <alexis.nouvel@neotys.com>
2019-02-18 09:31:41 +0100

    clean AssemblyInfo

--- Worksoft.Interfaces.NeoLoad.wsTransferToNeoLoad/wsTransferToNeoLoad/Properties/AssemblyInfo.cs
+++ Worksoft.Interfaces.NeoLoad.wsTransferToNeoLoad/wsTransferToNeoLoad/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("wsNeoLoad")]
+[assembly: AssemblyTitle("wsTransferToNeoLoad")]
 [assembly: AssemblyDescription("Integration between NeoLoad and Worksoft Certify")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Worksoft")]
-[assembly: AssemblyProduct("wsNeoLoad")]
+[assembly: AssemblyProduct("wsTransferToNeoLoad")]
 [assembly: AssemblyCopyright("Copyright © Neotys 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
